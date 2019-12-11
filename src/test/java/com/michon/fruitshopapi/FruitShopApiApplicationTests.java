15b60073367bd720028439f8120765b6071eb14b
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-12-11 21:29:06 +0100

    Initial commit

---
+++ src/test/java/com/michon/fruitshopapi/FruitShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michon.fruitshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
