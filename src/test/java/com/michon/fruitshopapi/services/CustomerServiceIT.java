8289664c80a640764d15bd5f89e23716a0a810d7
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2020-01-15 18:57:02 +0100

    Write test for PATCH method in service

--- src/test/java/com/michon/fruitshopapi/services/CustomerServiceIT.java
+++ src/test/java/com/michon/fruitshopapi/services/CustomerServiceIT.java
@@ -1,13 +1,18 @@
 package com.michon.fruitshopapi.services;
 
+import com.michon.fruitshopapi.domain.Customer;
 import com.michon.fruitshopapi.repositories.CategoryRepository;
 import com.michon.fruitshopapi.repositories.CustomerRepository;
 import com.michon.fruitshopapi.runner.BootRunner;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -36,5 +41,27 @@ public class CustomerServiceIT {
                .stream().findFirst()
                .map(x->x.getId())
                .get();
+    }
+
+    @Test
+    public void patchCustomerUpdateFirstNAmeTest(){
+        String updatedName = "NewName";
+        long id = getCustomerIdValue();
+
+        Customer previousCustomer = customerRepository.getOne(id);
+        assertNotNull(previousCustomer);
+
+        String previousLastName = previousCustomer.getLastName();
+
+        Customer customer = new Customer();
+        customer.setFirstName(updatedName);
+
+        customerService.patchCustomer(id, customer);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getFirstName());
+        assertEquals(previousLastName, updatedCustomer.getLastName());
     }
 }
